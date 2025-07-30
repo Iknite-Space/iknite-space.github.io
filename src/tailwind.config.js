3165f694069730f58c8f7ff46e9469ce131f0fbe
Ryan J. Yoder <ryan@iknite.studio>
2025-07-31 00:57:36 +0100

    WIP

---
+++ src/tailwind.config.js
@@ -0,0 +1,66 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+// tailwind.config.js
+module.exports = {
+  content: ['./layouts/**/*.html', './content/**/*.md'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'primary': colors.pink,
+        'secondary': colors.rose,
+        'neutral': colors.blue
+        // To change these, use https://www.tailwindshades.com/ with https://tailwindcss.com/docs/customizing-colors or create your own custom colors.
+      },
+      lineHeight: {
+        'extra-loose': '2.5',
+        '12': '3rem',
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-body': theme('colors.primary[800]'),
+            '--tw-prose-headings': theme('colors.primary[900]'),
+            '--tw-prose-lead': theme('colors.primary[700]'),
+            '--tw-prose-links': theme('colors.primary[900]'),
+            '--tw-prose-bold': theme('colors.primary[900]'),
+            '--tw-prose-counters': theme('colors.primary[600]'),
+            '--tw-prose-bullets': theme('colors.primary[400]'),
+            '--tw-prose-hr': theme('colors.primary[300]'),
+            '--tw-prose-quotes': theme('colors.primary[900]'),
+            '--tw-prose-quote-borders': theme('colors.primary[300]'),
+            '--tw-prose-captions': theme('colors.primary[700]'),
+            '--tw-prose-code': theme('colors.secondary[500]'),
+            '--tw-prose-pre-code': theme('colors.secondary[300]'),
+            '--tw-prose-pre-bg': theme('colors.gray[900]'),
+            '--tw-prose-th-borders': theme('colors.primary[300]'),
+            '--tw-prose-td-borders': theme('colors.primary[200]'),
+            '--tw-prose-invert-body': theme('colors.primary[200]'),
+            '--tw-prose-invert-headings': theme('colors.white'),
+            '--tw-prose-invert-lead': theme('colors.primary[300]'),
+            '--tw-prose-invert-links': theme('colors.secondary[400]'),
+            '--tw-prose-invert-bold': theme('colors.white'),
+            '--tw-prose-invert-counters': theme('colors.primary[400]'),
+            '--tw-prose-invert-bullets': theme('colors.primary[200]'),
+            '--tw-prose-invert-hr': theme('colors.primary[500]'),
+            '--tw-prose-invert-quotes': theme('colors.primary[100]'),
+            '--tw-prose-invert-quote-borders': theme('colors.primary[700]'),
+            '--tw-prose-invert-captions': theme('colors.primary[400]'),
+            '--tw-prose-invert-code': theme('colors.secondary[400]'),
+            '--tw-prose-invert-pre-code': theme('colors.secondary[300]'),
+            '--tw-prose-invert-pre-bg': theme('colors.gray[900]'),
+            '--tw-prose-invert-th-borders': theme('colors.primary[100]'),
+            '--tw-prose-invert-td-borders': theme('colors.primary[500]'),
+          },
+        },
+      }),
+    },
+  },
+  variants: {
+    typography: ["dark"],
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
